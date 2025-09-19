1be1b676356f9caffafc23e261082e312e035ec5
William-parte <wromerohiguera.22@gmail.com>
2025-09-18 23:08:09 -0500

    crud pedido listo y claiente nice :D

--- confi/confi.php
+++ confi/confi.php
@@ -1,8 +1,13 @@
 <?php
 
-
+// url de baquero de la tabla de producto//
 $urlCliente ="http://localhost:8080/cliente";
  $url="http://localhost:8080/producto";
+
+
+
+//url de william de la tabla de pedido//
+$urlPedido = "http://localhost:8080/pedido";
 
 
 ?>
